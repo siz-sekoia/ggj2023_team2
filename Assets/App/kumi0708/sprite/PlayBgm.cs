4a075af56c03be5df260fcc560bc7862e31597f1
shirai708 <naoya.shirai@craftar.co.jp>
2023-02-05 01:18:34 +0900

    bgm再生

--- Assets/App/kumi0708/sprite/PlayBgm.cs
+++ Assets/App/kumi0708/sprite/PlayBgm.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class PlayBgm : MonoBehaviour
 {
+    [SerializeField]
+    private string playBgmString = "rpg_01_loop";
+    [SerializeField]
+    private bool isLoop = true;
+    [SerializeField]
+    private float volume = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
-        AudioManager.Instance.PlayBGM("rpg_01_loop",true,0.2f);
+        AudioManager.Instance.PlayBGM(playBgmString, isLoop, volume);
     }
 }
