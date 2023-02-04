acae756755ef568cb8b123e2c4b1e32276b87c39
kenta.iiyama <kenta.iiyama@app-studio.jp>
2023-02-04 21:19:45 +0900

    修正コミット

--- Assets/App/AppRoot/Scripts/GameMain/LineController.cs
+++ Assets/App/AppRoot/Scripts/GameMain/LineController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI.Extensions;
 
@@ -10,15 +11,23 @@ namespace App
         [SerializeField] private PointController _pointController;
         [SerializeField] private UILineRenderer _uiLineRenderer;
 
+        [SerializeField] private TextMeshProUGUI _text;
+
         public PointController Point => _pointController;
         private readonly List<Vector2> _points = new();
         public bool IsReft;
         private int _nowIndex = 1;
         private bool initEnd;
 
-        public bool _isStop;
+        public bool IsStop;
+        public bool IsPause;
 
         public int Index;
+
+        public void SetText(int num)
+        {
+            _text.SetText(num.ToString());
+        }
 
         public void Setup(int index)
         {
@@ -42,12 +51,19 @@ namespace App
         {
             Debug.Log("Stop");
             _pointController.Stop();
-            _isStop = true;
+            IsStop = true;
+        }
+
+        public void SetPause(bool enable)
+        {
+            _pointController.SetPause(enable);
+            IsPause = enable;
         }
 
         private void Update()
         {
-            if (_isStop) return;
+            if (IsPause) return;
+            if (IsStop) return;
             if (!initEnd) return;
             _uiLineRenderer.Points[_nowIndex] = _pointController.transform.localPosition;
             _uiLineRenderer.SetAllDirty();
